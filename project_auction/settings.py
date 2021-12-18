586473d4067ba3129360ef487505de6002e968e9
Shams E Shifat <shamseshifat@gmail.com>
2021-12-18 22:52:49 +0600

    heroku configs

--- project_auction/settings.py
+++ project_auction/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h=pd%i(@1h!twgut5a26nfgin+4lhk#3%8t+ee)qyr!q#f42zp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'shifats-auction-app.herokuapp.com',
+    '127.0.0.1',
+]
 
 
 # Application definition
